25b6e57b6d4fabf52b39062ad31cd0e2a718aab5
Amy Cen <amy.cen@blackrock.com>
2019-04-20 06:19:42 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,5 +2,5 @@ def alphabetize(arr)
   # code here
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   ASCII_ALPHA = "abcdefghijklmnopqrstuvwxyz{|"
-
+  arr.sort_by{|string| string.tr(ESPERANTO_ALPHABET, ASCII_ALPHA)}
 end
